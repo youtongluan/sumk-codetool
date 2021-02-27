19184904d5b44b553937d61fd6c19722acece04c
youtl <3205207767@qq.com>
2021-02-27 20:45:53 +0800

    299

---
+++ sumk-codetool-test/src/main/java/org/git/member/constant/DemoUserConst.java
@@ -0,0 +1,26 @@
+package org.git.member.constant;
+
+public interface DemoUserConst {
+	
+	/**
+	 * 
+	 */
+	String id = "id";
+	
+	/**
+	 * 
+	 */
+	String name = "name";
+	
+	/**
+	 * 
+	 */
+	String age = "age";
+	
+	/**
+	 * 最后更新时间
+	 */
+	String lastUpdate = "lastUpdate";
+	
+	
+}
---
+++ sumk-codetool-test/src/main/java/org/git/member/constant/MultikeyConst.java
@@ -0,0 +1,26 @@
+package org.git.member.constant;
+
+public interface MultikeyConst {
+	
+	/**
+	 * 
+	 */
+	String id1 = "id1";
+	
+	/**
+	 * 
+	 */
+	String id2 = "id2";
+	
+	/**
+	 * 姓名
+	 */
+	String name = "name";
+	
+	/**
+	 * 
+	 */
+	String age = "age";
+	
+	
+}
