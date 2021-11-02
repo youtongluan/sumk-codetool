b98e2cde4961cf4f6d38ec97d1f0f880f44c0b36
youtl <3205207767@qq.com>
2021-11-02 19:14:36 +0800

    修改协议为mib

--- sumk-codetool/src/main/java/org/yx/db/sql/DBTableMaker.java
+++ sumk-codetool/src/main/java/org/yx/db/sql/DBTableMaker.java
@@ -1,18 +1,3 @@
-/**
- * Copyright (C) 2016 - 2030 youtongluan.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * 		http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 package org.yx.db.sql;
 
 import java.math.BigDecimal;
@@ -58,7 +43,7 @@ public class DBTableMaker {
 		}
 		StringBuilder sb = new StringBuilder();
 		sb.append("CREATE TABLE ").append(name(pm.getTableName())).append(" (\n\t");
-		List<ColumnMeta> cms = pm.fieldMetas;
+		List<ColumnMeta> cms = pm.fieldMetas();
 		ItemJoiner joiner = ItemJoiner.create(",\n\t", null, null);
 		Set<String> columnNames=new HashSet<>();
 		for (ColumnMeta cm : cms) {
